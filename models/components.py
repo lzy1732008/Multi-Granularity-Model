6a0d41363a303772104023cf487f3819e3cd269d
lzy1732008 <lzy1732008@163.com>
2019-11-14 15:36:36 +0800

    run multiGranularityStacking train v1 update

--- models/components.py
+++ models/components.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 def genericAttention(X,Y):
-    with tf.variable_scope("generic attention"):
+    with tf.variable_scope("generic-attention"):
         dot_matrix = tf.matmul(X,tf.transpose(Y,[0,2,1]))
         x_2_y = tf.nn.softmax(dot_matrix, axis=2)  # x对y每个词的关注度
         y_2_x = tf.nn.softmax(dot_matrix, axis=1)  # y对x每个词的关注度
