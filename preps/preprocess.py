eb682f44dc576c69865762cc6cf323054926103d
lzy1732008 <lzy1732008@163.com>
2019-12-11 14:36:50 +0800

    train MultiGranularityCNNQHJ_4 v2

--- preps/preprocess.py
+++ preps/preprocess.py
@@ -71,37 +71,41 @@ def setUp_inputs_QHJ(trainPath = None, valPath = None, testPath = None, rfModel=
     test = ""
     val = ""
     if trainPath:
-        args = []
-        tars = []
-        for i in range(15):
-            args.append((trainPath,wordEmbedding, wordVocab, rfModel,i * 1000,i * 1000 + 1000,1))
-            tars.append(_setUp_inputs_QHJ)
-        starttime = time.time()
-        mp = MultiProcess(tar=tars,arg=args)
-        mp.multi_processing()
-        train = list(q1.get())
-        endtime = time.time()
-        print("cost time:"+str(endtime-starttime))
+        # args = []
+        # tars = []
+        # for i in range(1):
+        #     args.append((trainPath,wordEmbedding, wordVocab, rfModel,i * 1000,i * 1000 + 1000,1))
+        #     tars.append(_setUp_inputs_QHJ)
+        # starttime = time.time()
+        # mp = MultiProcess(tar=tars,arg=args)
+        # mp.multi_processing()
+        # train = list(q1.get())
+        # endtime = time.time()
+        # print("cost time:"+str(endtime-starttime))
+        train = _setUp_inputs_QHJ(trainPath, wordEmbedding, wordVocab, rfModel, 0, 15000,1)
 
     if testPath:
-        args = []
-        tars = []
-        for i in range(10):
-            args.append((testPath, wordEmbedding, wordVocab, rfModel, i * 100, i * 100 + 100,2))
-            tars.append(_setUp_inputs_QHJ)
-        mp = MultiProcess(tar=tars, arg=args)
-        mp.multi_processing()
-        test = list(q2.get())
+        # args = []
+        # tars = []
+        # for i in range(1):
+        #     args.append((testPath, wordEmbedding, wordVocab, rfModel, i * 100, i * 100 + 100,2))
+        #     tars.append(_setUp_inputs_QHJ)
+        # mp = MultiProcess(tar=tars, arg=args)
+        # mp.multi_processing()
+        # test = list(q2.get())
+
+        test = _setUp_inputs_QHJ(testPath, wordEmbedding, wordVocab, rfModel, 0, 1000, 2)
 
     if valPath:
-        args = []
-        tars = []
-        for i in range(10):
-            args.append((valPath, wordEmbedding, wordVocab, rfModel, i * 100, i * 100 + 100,3))
-            tars.append(_setUp_inputs_QHJ)
-        mp = MultiProcess(tar=tars, arg=args)
-        mp.multi_processing()
-        val = list(q3.get())
+        # args = []
+        # tars = []
+        # for i in range(1):
+        #     args.append((valPath, wordEmbedding, wordVocab, rfModel, i * 100, i * 100 + 100,3))
+        #     tars.append(_setUp_inputs_QHJ)
+        # mp = MultiProcess(tar=tars, arg=args)
+        # mp.multi_processing()
+        # val = list(q3.get())
+        test = _setUp_inputs_QHJ(valPath, wordEmbedding, wordVocab, rfModel, 0, 1000, 3)
 
     env = {'train': train, 'test': test, 'val': val}
     return env
@@ -142,13 +146,14 @@ def _setUp_inputs_QHJ(sourcePath, wordEmbedding, wordVocab,rfModel,start,end,fla
             label = items[3]
             result.append([fact_input, law_input, law_label_input, label])
             count += 1
-            print("precessing {0}/{1} samples".format(count,len(lines)))
+            # print("precessing {0}/{1} samples".format(count,len(lines)))
     if flag == 1:
         q1.put(result)
     elif flag == 2:
         q2.put(result)
     elif flag == 3:
         q3.put(result)
+    return result
 
 def processTextWithoutDict(line,wordEmbedding, wordVocab):
     initContent = line.strip()
