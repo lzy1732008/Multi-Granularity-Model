9b549a0c7b94ac7165243f61071351256f3451f2
lzy1732008 <lzy1732008@163.com>
2020-01-04 11:41:09 +0800

    test MGCQ_24 v3 interaction1-layer add "zero-interaction-layer" and "fusion-layer-0"

--- models/MGCQ_24.py
+++ models/MGCQ_24.py
@@ -15,13 +15,23 @@ class MultiGraConfig:
     # mlp_output = 64
 
     #v2
+    # X_maxlen = 30
+    # Y_maxlen = 50
+    # dropout_rate = 0.5
+    # first_kernel_size = 2
+    # second_kernel_size = 4
+    # third_kernel_size = 8
+    # filters_num = 64
+    # mlp_output = 64
+
+    #v3
     X_maxlen = 30
     Y_maxlen = 50
     dropout_rate = 0.5
     first_kernel_size = 2
     second_kernel_size = 4
     third_kernel_size = 8
-    filters_num = 64
+    filters_num = 100
     mlp_output = 64
 
 
@@ -42,6 +52,19 @@ class MultiGranularityCNNModel:
         self.build_model()
 
     def build_model(self):
+        # with tf.variable_scope("related-score-layer"):
+        #     beta = tf.Variable(tf.random_normal(shape=[param.BaseConfig.word_dimension, 2], stddev=0, seed=1, dtype=tf.float32), trainable=True,
+        #                        name='beta')
+        #     self.weigt = tf.sigmoid(tf.einsum('abc,cd->abd', self.input_X2, beta))  # [B,l2,2]
+            # ks = tf.reduce_sum(weigt * self.x2_label, axis=-1)  # [B,l2]
+            # self.ks_rep = tf.reshape(tf.keras.backend.repeat_elements(ks, rep=self.config.X_maxlen, axis=1),
+            #                      shape=[-1, self.config.X_maxlen, self.config.Y_maxlen])
+
+            # self.weigt = tf.sigmoid(tf.einsum('abc,cd->abd', self.input_X2, beta))  # [B,l2,2]
+            # self.weight = tf.reduce_sum(weigt * self.x2_label, axis=-1)  # [B,l2]
+            # self.ks_rep = tf.reshape(tf.keras.backend.repeat_elements(self.weight, rep=self.config.X_maxlen, axis=1),
+            #                      shape=[-1, self.config.X_maxlen, self.config.Y_maxlen])
+
         with tf.variable_scope("zero-interaction-layer"):
             self.inter_0 = self.interaction(self.input_X1,self.input_X2)
             self.inter_rep_0 = tf.reshape(
@@ -57,20 +80,6 @@ class MultiGranularityCNNModel:
                                                    name='fusion-fnn')
             # self.fusion_output_1 = tf.layers.dropout(self.fusion_output_1,rate=self.dropout_rate)
             self.fusion_output_max_0 = tf.reduce_max(self.fusion_output_0, axis=-1)
-
-        # with tf.variable_scope("related-score-layer"):
-        #     beta = tf.Variable(tf.random_normal(shape=[param.BaseConfig.word_dimension, 2], stddev=0, seed=1, dtype=tf.float32), trainable=True,
-        #                        name='beta')
-            # weigt = tf.einsum('abc,cd->abd', self.input_X2, beta)  # [B,l2,2]
-            # ks = tf.reduce_sum(weigt * self.x2_label, axis=-1)  # [B,l2]
-            # self.ks_rep = tf.reshape(tf.keras.backend.repeat_elements(ks, rep=self.config.X_maxlen, axis=1),
-            #                          shape=[-1, self.config.X_maxlen, self.config.Y_maxlen])
-
-            # self.weigt = tf.sigmoid(tf.einsum('abc,cd->abd', self.input_X2, beta))  # [B,l2,2]
-            # self.weight = tf.reduce_sum(weigt * self.x2_label, axis=-1)  # [B,l2]
-            # self.ks_rep = tf.reshape(tf.keras.backend.repeat_elements(self.weight, rep=self.config.X_maxlen, axis=1),
-            #                          shape=[-1, self.config.X_maxlen, self.config.Y_maxlen])
-
 
         with tf.variable_scope("first-CNN-layer"):
             self.output_x1_1 = tf.layers.conv1d(self.input_X1,filters=self.config.filters_num,kernel_size=self.config.first_kernel_size,padding='same',name='first-cnn1')
