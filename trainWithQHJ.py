274984b420aabb5dc352778a38672750a58c441e
lzy1732008 <lzy1732008@163.com>
2020-01-04 19:17:36 +0800

    train MGCQ_24 v3 interaction1-layer add "zero-interaction-layer" and "fusion-layer-0" and use inter14 in inter-layer-0

--- trainWithQHJ.py
+++ trainWithQHJ.py
@@ -281,9 +281,9 @@ def run_mutli():
     # train_data, val_data, test_data = data_load(None, None,
     #                                             param.BaseConfig.testPath, model, rf)
     print('train data shape:{0}\n val data shape:{1}\n test data shape:{2}'.format(len(train_data), len(val_data), len(test_data)))
-    # for i in range(5):
-    #     Path = basicPath(i)
-    #     train(train_data,val_data,Path)
+    for i in range(5):
+        Path = basicPath(i)
+        train(train_data,val_data,Path)
 
 
 
