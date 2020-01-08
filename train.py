088bd6938b63bef047e6ecbbfbc723831dd5c79d
lzy1732008 <lzy1732008@163.com>
2020-01-08 11:28:51 +0800

    test MGCQ_16 add inter0layer

--- train.py
+++ train.py
@@ -264,7 +264,7 @@ def checkPrediction(pred_cls, target_y,probs):
     with open('resource/预测结果分析/MGCQ_23-qj.json','w',encoding='utf-8') as fw:
         json.dump(law_result,fw)
 
-train()
+# train()
 y_test_cls,y_pred_cls = test()
 
 
