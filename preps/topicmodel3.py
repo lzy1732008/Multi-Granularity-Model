80834e5923d4ed73e304799bd406d9658c11ca87
lzy1732008 <lzy1732008@163.com>
2020-01-08 11:16:30 +0800

    train MGCQ_16 add inter0layer

--- preps/topicmodel3.py
+++ preps/topicmodel3.py
@@ -1,5 +1,5 @@
 import codecs
-
+import jieba
 from gensim import corpora
 from gensim.models import LdaModel
 from gensim.corpora import Dictionary
@@ -45,39 +45,35 @@ def train(corpuspath,modelpath):
     #     print("%s\t%f\n" % (lda.print_topic(topic[0]), topic[1]))
 
 
-# import os
-# dir_path = '/Users/wenny/nju/task/LawDocumentAna/2014filled/2014'
-# files = os.listdir(dir_path)
-# stopwords = codecs.open('stopWords/1893（utf8）.txt','r',encoding='utf8').readlines()
-# stopwords = [ w.strip() for w in stopwords]
-# output_w = open('result/facts_corpus.txt','w',encoding='utf-8')
-# output_lines = ''
-# for file in files:
-#     if not file.endswith('.xml'): continue
-#     wspath = os.path.join(dir_path,file)
-#     fact = getRDSS(wspath)
-#     fact_split = ' '.join(list(filter(lambda x:x not in stopwords,jieba.lcut(fact)))) + '\n'
-#     output_lines += fact_split
-# output_w.write(output_lines)
 import os
 corpus_path = '../resource/facts_corpus.txt'
-model_path = '../result/lda/fact_lda-10.model'
-train(corpus_path,model_path)
+# model_path = '../result/lda/fact_lda-10.model'
+# train(corpus_path,model_path)
 
-
-# def predictTopic(input):
-#     lda = LdaModel.load('../result/lda/fact_lda-10.model')
-#     topic_words = open('../result/lda/fact_lad_print-10.txt', 'w', encoding='utf-8')
-#     print_str = ''
-#     for topic in lda.print_topics(num_words=100):
-#         termNumber = topic[0]
-#         listOfTerms = topic[1].split('+')
-#         # for term in listOfTerms:
-#         #     listItems = term.split('*')
-#         #     # print(listItems)
-#         #     print('  ', listItems[1], '(', listItems[0], ')', sep='')
-#         print_str += topic[1] + '\n'
-#     topic_words.write(print_str)
-#     topic_words.close()
 #
-# predictTopic("")
+# def predictTopic():
+#     corpus_path = '../resource/facts_corpus.txt'
+#     train = []
+#     fp = codecs.open(corpus_path, 'r', encoding='utf8')
+#     for line in fp:
+#         line = line.strip()
+#         if line == '': continue
+#         line = line.split()
+#         train.append([w for w in line])
+#
+#     dictionary = corpora.Dictionary(train)
+#     lda = LdaModel.load('../result/lda/fact_lda-10.model')
+#     for line in train[:100]:
+#         test_doc = line
+#         doc_bow = dictionary.doc2bow(test_doc)  # 文档转换成bow
+#         doc_lda = lda[doc_bow]  # 得到新文档的主题分布
+#         # 输出新文档的主题分布
+#         print(doc_lda)
+#         print('==================================================')
+#         print(line)
+#         for topic in doc_lda:
+#             print("%s\t%f\n" % (lda.print_topic(topic[0]), topic[1]))
+
+
+#
+# predictTopic()
