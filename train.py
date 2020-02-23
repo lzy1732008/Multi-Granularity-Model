acba8dfd5bd25d7e861268b9120ae13b4ce7045d
lzy1732008 <lzy1732008@163.com>
2020-02-23 09:52:48 +0800

    train Arc2 for comparison experiment of jtzs

--- train.py
+++ train.py
@@ -11,23 +11,23 @@ import sys
 import pickle
 import numpy as np
 
-from models.arc_1 import MultiGraConfig, MultiGranularityCNNModel  #修改处
+from models.baselines.Arc_2 import modelConfig, ARC2model  #修改处
 from preps.data_load import data_load_lawone,data_load_test_lawone
 from preps.data_load_generic import get_batch_data,get_batch_data_test
 from util.feedDict import feed_data_4 as feed_data_fun  #修改处
 from util.evaluate import evaluate_1 as evaluate_fun
 from util.evaluate import wsevaluate
-from models.parameter import BasicConfig2 as basic_config
+from models.parameter import BaseConfig as basic_config
 
 
-save_dir = 'result/model/Arc_1'  #修改处
-param_des = 'gyshz-times3'
+save_dir = 'result/model/Arc_2'  #修改处
+param_des = 'jtzs-times1'
 # param_des = 'initparam-qj'
 save_path = os.path.join(save_dir,param_des+'/checkpoints/best_validation')
 tensorboard_dir = os.path.join(save_dir,param_des+'/tensorboard')
 
-model_config = MultiGraConfig()
-model = MultiGranularityCNNModel()
+model_config = modelConfig()
+model = ARC2model(modelConfig)
 
 with open(basic_config.rf_model_path, 'rb') as fr:
     rf = pickle.load(fr)
